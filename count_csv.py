f0f65efb4e33539d8442a1a1782fc7624aa214d2
Ulugbek Salaev <82821757+UlugbekSalaev@users.noreply.github.com>
2026-03-26 11:07:29 +0500

    Initial release of UzMorph

--- count_csv.py
+++
@@ -1,7 +0,0 @@
-import csv, os
-dp = os.path.join(os.path.dirname(__file__), 'uzmorph', 'data')
-for f in sorted(os.listdir(dp)):
-    if f.endswith('.csv'):
-        with open(os.path.join(dp, f), encoding='utf-8') as fh:
-            rows = sum(1 for _ in csv.reader(fh)) - 1
-        print(f'{f}: {rows} entries')
